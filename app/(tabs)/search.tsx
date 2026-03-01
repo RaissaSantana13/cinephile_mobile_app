441fd69a9de14ff26523d1f7e8d9a21d0acd08d9
Raissa Santana <raissasanta8@gmail.com>
2026-03-01 16:17:04 -0300

    searchbar_navigationbar

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Search = () => {
+    return (
+        <View>
+            <Text>Your Content</Text>
+        </View>
+    );
+};
+
+export default Search;
